a033e53dae94ef429e88fd2420ba304370c05f97
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-13 22:15:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,25 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
- function divide(arr, maxSum) {
-    if (!Array.isArray(arr) || arr.length === 0 || typeof maxSum !== 'number') {
-      throw new Error('Invalid input');
-    }
-  
-    let result = [];
+  const result = [];
     let currentSubarray = [];
-  
-    for (let i = 0; i < arr.length; i++) {
-      const currentElement = arr[i];
-  
-      if (currentSubarray.reduce((sum, num) => sum + num, 0) + currentElement <= maxSum) {
-        currentSubarray.push(currentElement);
-      } else {
-        result.push([...currentSubarray]);
-        currentSubarray = [currentElement];
-      }
+    let currentSum = 0;
+
+    for (let num of arr) {
+        if (currentSum + num <= n) {
+            currentSubarray.push(num);
+            currentSum += num;
+        } else {
+            result.push([...currentSubarray]);
+            currentSubarray = [num];
+            currentSum = num;
+        }
     }
-  
+
     if (currentSubarray.length > 0) {
-      result.push(currentSubarray);
+        result.push([...currentSubarray]);
     }
-  
     return result;
-  }
 };
 
 const n = prompt("Enter n: ");
